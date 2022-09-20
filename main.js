7174f8a901f13b1713343059eb62586da7f4a48c
Harold Wolfinger <harold.wolfinger@exelaration.com>
2022-09-20 13:49:56 -0400

    add options to select, add funcs & event listener added handler, two variables, able to see heroku json array

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
+console.log('hello')
+
+function prommycountry (response) {
+    console.log(response.json());
+}
+
+function handler () {
+    console.log(document);
+    const dropdown = document.getElementById('country');
+    console.log(dropdown);
+    while (dropdown.length > 0) {                
+    dropdown.remove(0);
+    }
+    const promi = fetch('https://xc-countries-api.herokuapp.com/api/countries/');
+    console.log(promi);
+    promi.then(prommycountry);
+}
+
+document.addEventListener ('DOMContentLoaded', handler);
+
+
+/*
+fetch('https://xc-countries-api.herokuapp.com/api/countries/<country_code>/states/')
+//*/
+
+
+
+
+
+
+
+/*
+
 // initial attempt at using fetch method (code via https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json)
-let dropdown = document.getElementById('country');
-dropdown.length = 0;
+
 
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Please select country';
