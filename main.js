f90df1b373971ef428830eb3386928d622f1be88
Harold Wolfinger <harold.wolfinger@exelaration.com>
2022-09-20 14:11:46 -0400

    remove testing.js add comments main.js

--- main.js
+++ main.js
@@ -11,9 +11,13 @@ function handler () {
     while (dropdown.length > 0) {                
     dropdown.remove(0);
     }
+    // to queue the network call 
     const promi = fetch('https://xc-countries-api.herokuapp.com/api/countries/');
+    // promi represents the promise that the browser will fulfill the request
     console.log(promi);
+    // telling computer that when it is resolved, call prommy country
     promi.then(prommycountry);
+    console.log('i did it!');
 }
 
 document.addEventListener ('DOMContentLoaded', handler);
--- testing.js
+++
@@ -1,3 +0,0 @@
-console.log('hello')
-fetch('https://xc-countries-api.herokuapp.com/api/countries/');
-fetch('https://xc-countries-api.herokuapp.com/api/countries/<country_code>/states/');
